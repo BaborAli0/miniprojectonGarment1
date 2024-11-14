7328fa9eab5875b4ffd292433faba9ddadb54263
BaborAli0 <ali2305101954@diu.edu.bd.com>
2024-11-14 09:41:12 +0600

    1st added in 2nd commit.

--- src/miniproject_garment1/Order.java
+++ src/miniproject_garment1/Order.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package miniproject_garment1;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-/**
- *
- * @author alimh
- */
 public class Order {
-    
+
+    private String orderId;
+    private Date orderDate;
+    private List<Garment> garmentList;
+    private double totalAmount;
+
+    public Order(String orderId, Date orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.garmentList = new ArrayList<>();
+        this.totalAmount = 0.0;
+    }
+
+    public void addGarment(Garment garment) {
+        garmentList.add(garment);
+    }
+
+    public void calculateTotalAmount() {
+        totalAmount = garmentList.stream()
+                .mapToDouble(garment -> garment.getQuantity() * garment.getPrice())
+                .sum();
+    }
+
+    public void printOrderDetails() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Order Date: " + orderDate);
+        System.out.println("Garments:");
+        for (Garment garment : garmentList) {
+            System.out.println("  - " + garment.getName() + " (Qty: " + garment.getQuantity() + ", Price: " + garment.getPrice() + ")");
+        }
+        System.out.println("Total Amount: " + totalAmount);
+    }
+
+  
+    public double getTotalAmount() {
+        return totalAmount;
+    }
 }
