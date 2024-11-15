9665be3bf24ab9b189139565b501f306eebb8b5d
BaborAli0 <ali2305101954@diu.edu.bd.com>
2024-11-15 12:19:02 +0600

    1st added in 3rd commit.

---
+++ src/miniproject_garment1/Customer.java
@@ -0,0 +1,58 @@
+import java.util.List;
+
+public class Customer {
+    
+    private String customerID;
+    private String customerName;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public Customer(String customerID, String customerName, String emailAddress, String phoneNumber) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public List<Order> viewOrders() {
+      
+        return null;
+    }
+
+    public void placeOrder(Order order) {
+     
+    }
+}
